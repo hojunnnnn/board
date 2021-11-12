ce752df6b00f03c6319564bf6c294f985021c9fd
hojunnnnn <gotjd8515@gmail.com>
2021-11-12 16:53:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/coco/board/BoardApplication.java
@@ -0,0 +1,20 @@
+package com.coco.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BoardApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoardApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coco/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coco.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
