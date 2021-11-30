77839597b150e4a02cbdf01160eba092afcc371e
hojunnnnn <gotjd8515@gmail.com>
2021-11-30 19:08:17 +0900

    게시글 상세/수정/글작성 페이지 UI 변경 / 댓글 UI 구현중 (미완성) / js 게시글 수정/삭제 처리 부분수정

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -41,45 +41,48 @@ const main = {
             title: $('#title').val(),
             content: $('#content').val()
         };
-
         const id = $('#id').val();
+        const con_check = confirm("수정하시겠습니까?");
 
-        $.ajax({
-            type: 'PUT',
-            url: '/api/posts/'+id,
-            dataType: 'JSON',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function () {
-            const con_check = confirm("수정하시겠습니까?");
-            if (con_check == true) {
+        if(con_check == true) {
+            $.ajax({
+                type: 'PUT',
+                url: '/api/posts/'+id,
+                dataType: 'JSON',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function () {
+                alert("수정되었습니다.");
                 window.location.href = '/posts/read/' + id;
-            } else {
-                return ;
-            }
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        } else {
+            return false;
+        }
     },
 
     delete : function () {
         const id = $('#id').val();
+        const con_check = confirm("정말 삭제하시겠습니까?");
 
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/posts/'+id,
-            dataType: 'JSON',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function () {
-            const con_check = confirm("삭제하시겠습니까?");
-            if (con_check == true) {
+        if(con_check == true) {
+            $.ajax({
+                type: 'DELETE',
+                url: '/api/posts/'+id,
+                dataType: 'JSON',
+                contentType: 'application/json; charset=utf-8'
+
+            }).done(function () {
+                alert("삭제되었습니다.");
                 window.location.href = '/';
-            } else {
-                return ;
-            }
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        } else {
+            return false;
+        }
+
     }
 };
 
